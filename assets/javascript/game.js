42158beafba793b2f5030148345cf98f97c6fd3f
El Pirata Mike <mikefields81@gmail.com>
2017-11-12 22:36:16 -0600

    can't fix the issue to filter out letters recorded but all new letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,12 @@
 
 // set variables
 
-var wordArray = ["Pineapple", "Uzbekistan", "Candidate", "Tropical", "Competence", "Leftovers", "Overwhelm", "Psychology", "Conversation", "Broadcast"];
+var wordArray = ["pineapple", "gratification", "candidate", "tropical", "competence", "leftovers", "overwhelm", "psychology", "conversation", "broadcast"];
 var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
 var underScores = [];
 var wrongLetter = [];
 var guessesLeft = 7;
+var lettersUsed = [];
 
 
 
@@ -29,6 +30,8 @@ var guessesLeft = 7;
 
    				function startGame () {
 
+   					console.log(randomWord); 
+
    					underScores = [];
    					wrongLetter = [];
    					guessesLeft = 7;
@@ -37,7 +40,7 @@ var guessesLeft = 7;
 				{
 					underScores[i] = "_";
 				}
-				//sending correct number of underscores to the 
+				//sending correct number of underscores to the game
 
 				spaces = underScores.join(" ");
 				document.getElementById('blankWord').innerHTML = spaces;
@@ -45,9 +48,13 @@ var guessesLeft = 7;
 
 			}
 
+			//checks to see if a letter is pressed
+
 				document.onkeyup = function(event) {
 
 				var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+
 
 		if (userInput === "a" || userInput === "b" || userInput === "c" || userInput === "d" || userInput === "e" || userInput === "f"
 		|| userInput === "g" || userInput === "h" || userInput === "i" || userInput === "j" || userInput === "k" 
@@ -57,9 +64,46 @@ var guessesLeft = 7;
 
 				document.getElementById('warning').innerHTML = "";
 
+			for (var i = 0; i< randomWord.length; i++)
+
+			{ //if the letter selected is in the random word, print that letter to the html page, log in lettersUsed
+
+				if (userInput === randomWord[i].toLowerCase()) {
+
+					underScores.splice([i],1,userInput);
+					// lettersUsed.push(userInput);
+					spaces = underScores.join(" ");					
+					document.getElementById('blankWord').innerHTML = spaces;
+						console.log(underScores);
+						console.log(underScores.indexOf(userInput));
+		
+
 				}
 
-				else{
+			else { //if the letter selected is not in the randomWord and has not been guessed previously
+
+				if (wrongLetter.indexOf(userInput) ===-1 && underScores.indexOf(userInput) ===!-1)
+
+				{
+
+				wrongLetter.push(userInput);
+				noCommas = wrongLetter.join(" ");
+				document.getElementById('letterbin').innerHTML = noCommas;}
+	
+
+				
+
+			}
+
+			}
+
+
+		}
+
+
+				//if not a letter sends a warning to the html
+
+		else {
 
 					document.getElementById('warning').innerHTML = "This is a word guessing game, letters only please...";
 				}
